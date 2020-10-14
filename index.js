a31c0233875ffa568ee2a35f4b51eb016b142711
Alexander Heß <hess.alexander1@web.de>
2020-10-14 11:47:30 +0200

    + HTML Form to upload images + set marker at cursor position
    + nedb datastore
    + route to handle uploaded images

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
+const Datastore = require('nedb')
+const busboy = require('connect-busboy');
 
+
+app.use(busboy())
 const port = 3000;
-
+// Datastore localfile in the same directory
+const databse = new Datastore('database.db');
+databse.loadDatabase();
 
 app.listen(3000, () => {
     console.log('listening at 3000');
 })
 
 app.use(express.static('public'));
+
+
+// Handle Uploading Images here 
+app.post("/api", (request, response) => {
+
+   
+    request.pipe(request.busboy);
+    request.busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+        console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+        file.on('data', function(data) {
+          console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+        });
+        file.on('end', function() {
+          console.log('File [' + fieldname + '] Finished');
+        });
+  });
+
+
+    
+})
